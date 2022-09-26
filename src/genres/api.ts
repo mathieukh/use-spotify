1f08403dc346e713c4bcde597b3a465e8efc4434
Mathieu KHALEM <mathieu.khalem@gmail.com>
2022-09-26 11:01:57 +0200

    add genres api

---
+++ src/genres/api.ts
@@ -0,0 +1,12 @@
+import { spotifyClient } from "../api";
+
+/**
+ * Retrieve a list of available genres seed parameter values for recommendations.
+ * @returns {{ genres: string[] }} A set of genres
+ */
+export const getAvailableMarkets = async () => {
+  const response = await spotifyClient.get(
+    `/recommendations/available-genre-seeds`
+  );
+  return response.data as { genres: string[] };
+};
