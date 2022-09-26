11fddc0f9853c96eddc8582ffacdf99105a9e524
Mathieu KHALEM <mathieu.khalem@gmail.com>
2022-09-26 10:53:35 +0200

    add markets api

---
+++ src/markets/api.ts
@@ -0,0 +1,10 @@
+import { spotifyClient } from "../api";
+
+/**
+ * Get the list of markets where Spotify is available.
+ * @returns {{ markets: string[] }} A markets object with an array of country codes
+ */
+export const getAvailableMarkets = async () => {
+  const response = await spotifyClient.get(`/markets`);
+  return response.data as { markets: string[] };
+};
